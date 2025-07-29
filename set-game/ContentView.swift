94a9bc80632bfbf2ef6408f23d2e273cd47d0daf
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2025-07-29 15:25:27 -0500

    Initialize Xcode project structure and add essential files

---
+++ set-game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  set-game
+//
+//  Created by Juan Francisco Marcenaro Arellano on 29/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ set-game/set_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_gameApp.swift
+//  set-game
+//
+//  Created by Juan Francisco Marcenaro Arellano on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct set_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

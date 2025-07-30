9baa68aaa80a6e634e417b4320857a2c925c4ef1
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2025-07-29 19:17:07 -0500

    Refactor SetGameModel.swift by adding missing documentation and improving code formatting

--- set-game/Model/SetGameModel.swift
+++ set-game/Model/SetGameModel.swift
@@ -1,7 +1,25 @@
-//
-//  SetGameModel.swift
-//  set-game
-//
-//  Created by Juan Francisco Marcenaro Arellano on 29/07/25.
-//
+// Model/SetGameModel.swift
 
+import Foundation
+
+struct SetGameModel {
+    private(set) var deck: [SetCard] = []
+
+    mutating func generateDeck() {
+        deck = CardColor.allCases.flatMap { color in
+            CardSymbol.allCases.flatMap { symbol in
+                CardNumber.allCases.flatMap { number in
+                    CardShading.allCases.map { shading in
+                        SetCard(
+                            id: UUID(),
+                            color: color,
+                            symbol: symbol,
+                            shading: shading,
+                            number: number
+                        )
+                    }
+                }
+            }
+        }.shuffled()
+    }
+}
